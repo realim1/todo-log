9f63a47a1bfb5fcfed0f85cd2892a3d569c8aa97
Markus Realica <markus.developer2@gmail.com>
2021-09-26 21:48:02 -0700

    Add todo-log-item component and adjust testData for other case

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,20 @@
 import React, { useState } from "react";
-import { Container, Card, Nav } from "react-bootstrap";
+import { Container } from "react-bootstrap";
+import TodoLogItem from "./components/todo-log-item/todo-log-item.component";
 import "./App.scss";
 
 function App() {
 	const [testData, setTestData] = useState([
 		{
 			date: "1/2/22",
-			todos: ["replicate issue", "fix bugs", "write documentation"],
+			todos: [
+				"replicate issue",
+				"fix bugs",
+				"write documentation",
+				"replicate issue",
+				"fix bugs",
+				"write documentation",
+			],
 			completed: ["Resolved issue with application"],
 			blockers: ["Cannot access IT tickets", "Mouse was replaced with rat"],
 		},
@@ -20,40 +28,15 @@ function App() {
 			date: "1/4/22",
 			todos: ["replicate issue", "fix bugs", "write documentation"],
 			completed: ["Resolved issue with application"],
-			blockers: ["Cannot access IT tickets", "Mouse was replaced with rat"],
+			blockers: [],
 		},
 	]);
 
 	return (
-		<Container className='App'>
+		<Container>
 			<header className='my-3'>Todo Log</header>
-			{testData.map((item) => {
-				return (
-					<Card className='mb-4'>
-						<Card.Header>
-							<Nav variant='pills' defaultActiveKey='#first'>
-								<Nav.Item>
-									<Nav.Link href='#first'>Todos</Nav.Link>
-								</Nav.Item>
-								<Nav.Item>
-									<Nav.Link href='#completed'>Completed</Nav.Link>
-								</Nav.Item>
-								<Nav.Item>
-									<Nav.Link href='#blockers'>Blockers</Nav.Link>
-								</Nav.Item>
-							</Nav>
-						</Card.Header>
-						<Card.Body>
-							<Card.Title className='left-align'>Todo</Card.Title>
-							{item.todos.map((todo) => {
-								return <Card.Text>- {todo}</Card.Text>;
-							})}
-						</Card.Body>
-						<Card.Footer className='text-muted text-center'>
-							{item.date}
-						</Card.Footer>
-					</Card>
-				);
+			{testData.map((logItem, index) => {
+				return <TodoLogItem key={index} logItem={logItem} />;
 			})}
 		</Container>
 	);
---
+++ client/src/components/todo-log-item/todo-log-item.component.jsx
@@ -0,0 +1,79 @@
+import React, { useState } from "react";
+import { Card, Nav } from "react-bootstrap";
+
+import "./todo-log-item.style.scss";
+
+const TodoLogItem = ({ logItem }) => {
+	const [activeNav, setActiveNav] = useState("todos");
+
+	const buildCardBody = (item) => {
+		switch (activeNav) {
+			case "todos":
+				return (
+					<>
+						<Card.Title>Todos</Card.Title>
+						{item.todos.map((todo) => {
+							return <Card.Text>- {todo}</Card.Text>;
+						})}
+					</>
+				);
+			case "completed":
+				return (
+					<>
+						<Card.Title>Completed</Card.Title>
+						{item.completed.map((completedTasks) => {
+							return <Card.Text>- {completedTasks}</Card.Text>;
+						})}
+					</>
+				);
+			case "blockers":
+				return (
+					<>
+						<Card.Title>Blockers</Card.Title>
+						{item.blockers.length > 0 ? (
+							logItem.blockers.map((blocker) => {
+								return <Card.Text>- {blocker}</Card.Text>;
+							})
+						) : (
+							<Card.Text>No blockers</Card.Text>
+						)}
+					</>
+				);
+			default:
+				break;
+		}
+	};
+	return (
+		<Card className='mb-4'>
+			<Card.Header>
+				<Nav variant='pills' defaultActiveKey='#todos'>
+					<Nav.Item>
+						<Nav.Link href='#todos' onClick={() => setActiveNav("todos")}>
+							Todos
+						</Nav.Link>
+					</Nav.Item>
+					<Nav.Item>
+						<Nav.Link
+							href='#completed'
+							onClick={() => setActiveNav("completed")}>
+							Completed
+						</Nav.Link>
+					</Nav.Item>
+					<Nav.Item>
+						<Nav.Link href='#blockers' onClick={() => setActiveNav("blockers")}>
+							Blockers
+						</Nav.Link>
+					</Nav.Item>
+				</Nav>
+			</Card.Header>
+			<Card.Body style={{ minHeight: "200px" }}>
+				{buildCardBody(logItem)}
+			</Card.Body>
+			<Card.Footer className='text-muted text-center'>
+				{logItem.date}
+			</Card.Footer>
+		</Card>
+	);
+};
+
+export default TodoLogItem;
