9d0663613d0cc8e996e3df1652acf7fe708887d7
Markus Realica <markus.developer2@gmail.com>
2021-10-01 19:25:56 -0700

    Add form modal for adding new log items.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,16 @@
 import React, { useState } from "react";
-import { Container } from "react-bootstrap";
+import { Container, Button, Modal, Form } from "react-bootstrap";
 import TodoLogItem from "./components/todo-log-item/todo-log-item.component";
+import InputList from "./components/input-list/input-list.component";
 import "./App.scss";
 
 function App() {
+	const [todos, setTodos] = useState([""]);
+	const [completeds, setCompleteds] = useState([""]);
+	const [blockers, setBlockers] = useState([""]);
+
+	const [showAddItemModal, setShowAddItemModal] = useState(false);
+	const closeShowAddItemModal = () => setShowAddItemModal(false);
 	const [testData, setTestData] = useState([
 		{
 			date: "1/2/22",
@@ -32,12 +39,56 @@ function App() {
 		},
 	]);
 
+	const handleSubmit = (e) => {
+		const date = new Date();
+
+		const formatedDate =
+			date.getMonth() + 1 + "/" + date.getDate() + "/" + date.getFullYear();
+
+		e.preventDefault();
+		const newLog = {
+			date: formatedDate,
+			todos: todos.filter((item) => item),
+			completed: completeds.filter((item) => item),
+			blockers: blockers.filter((item) => item),
+		};
+		setTestData([...testData, newLog]);
+	};
+
 	return (
 		<Container>
 			<header className='my-3'>Todo Log</header>
+			<Button
+				className='text-center w-100 mb-3'
+				onClick={() => setShowAddItemModal(true)}>
+				Add Log Item +
+			</Button>
 			{testData.map((logItem, index) => {
 				return <TodoLogItem key={index} logItem={logItem} />;
 			})}
+			<Modal show={showAddItemModal}>
+				<Modal.Header>
+					<Modal.Title>Log Form</Modal.Title>
+				</Modal.Header>
+				<Form onSubmit={handleSubmit}>
+					<Modal.Body>
+						<InputList label='Todos' updateParent={setTodos} />
+						<InputList label='Completed' updateParent={setCompleteds} />
+						<InputList label='Blockers' updateParent={setBlockers} />
+					</Modal.Body>
+					<Modal.Footer>
+						<Button variant='secondary' onClick={closeShowAddItemModal}>
+							Close
+						</Button>
+						<Button
+							variant='primary'
+							type='submit'
+							onClick={closeShowAddItemModal}>
+							Submit
+						</Button>
+					</Modal.Footer>
+				</Form>
+			</Modal>
 		</Container>
 	);
 }
