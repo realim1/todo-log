8d1c10f52f62453ef6e40bdb4611c0b8848b3de6
Markus Realica <markus.developer2@gmail.com>
2021-10-03 19:43:48 -0700

    Add middle ware and handling of production/development node enviroments

--- server/index.js
+++ server/index.js
@@ -1,8 +1,31 @@
 const express = require("express");
+const morgan = require("morgan");
+const cors = require("cors");
+const dotenv = require("dotenv");
+
+dotenv.config({ path: "./config/config.env" });
 
 const app = express();
 
-const port = 8000;
+//Handle development
+if (process.env.NODE_ENV === "development") {
+	//Dev loggin
+	app.use(morgan("dev"));
+
+	//Access-Control-Allow-Origin: *
+	app.use(cors());
+}
+
+//Handle production
+if (process.env.NODE_ENV === "production") {
+	//Set static folder
+	app.use(express.static(__dirname + "/public/"));
+
+	//Handle SPA
+	app.get(/.*/, (req, res) => res.sendFile(__dirname + "/public/index.html"));
+}
+
+const port = process.env.PORT || 8000;
 
 app.listen(port, () => {
 	console.log(`Server is running on port ${port}`);
