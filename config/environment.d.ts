496d7a657825ce47df25d3e17699bcb05b0ac1c9
Markus Realica <markus.developer2@gmail.com>
2021-11-21 12:49:34 -0800

    Change CRYPT_KEY type from jwt.Secret to string.

--- config/environment.d.ts
+++ config/environment.d.ts
@@ -1,11 +1,10 @@
-import { Secret } from "jsonwebtoken";
 declare global {
 	namespace NodeJS {
 		interface ProcessEnv {
 			MONGODB_URI: string;
 			NODE_ENV: "development" | "production";
 			PORT?: string;
-			CRYPT_KEY: Secret;
+			CRYPT_KEY?: string;
 		}
 	}
 }
